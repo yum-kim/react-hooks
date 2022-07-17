87f61ca2c08e22329fc3d5cc6a10f40e544d2552
YumiKim <dwkimym93@gmail.com>
2022-07-17 23:05:01 +0900

    ✨ add useBeforeLeave

---
+++ useBeforeLeave/useBeforeLeave.js
@@ -0,0 +1,29 @@
+import React from "react";
+
+/**
+ * usePreventLeave
+ * 아직 저장하지 않았다고 알려주는 기능 (브라우저에서 사이트를 떠날건지 물어봄)
+ */
+const usePreventLeave = () => {
+  const listener = (event) => {
+    event.preventDefault();
+    event.returnValue = "";
+  };
+  const enablePrevent = () => window.addEventListener("beforeunload", listener);
+  const disablePrevent = () =>
+    window.removeEventListener("beforeunload", listener);
+
+  return { enablePrevent, disablePrevent };
+};
+
+const App = () => {
+  const { enablePrevent, disablePrevent } = usePreventLeave();
+  return (
+    <div className="App">
+      <button onClick={enablePrevent}>Protect</button>
+      <button onClick={disablePrevent}>Unprotect</button>
+    </div>
+  );
+};
+
+export default App;
