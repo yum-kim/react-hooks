ebe4ccceca4d4fd04a58d3615669cac65850318e
YumiKim <dwkimym93@gmail.com>
2022-07-29 00:00:49 +0900

    ✨ add useAxios

---
+++ useAxios/app.js
@@ -0,0 +1,25 @@
+import React from "react";
+import useAxios from "./useAxios";
+
+/**
+ * useAxios
+ *
+ */
+
+const App = () => {
+  const { loading, data, error, refetch } = useAxios({
+    url: "https://yts.mx/api/v2/list_movies.json"
+  });
+
+  console.log(loading, data, error);
+
+  return (
+    <div className="App">
+      <h1>{data && data.status}</h1>
+      <h2>{loading && "Loading"}</h2>
+      <button onClick={refetch}>Refetch</button>
+    </div>
+  );
+};
+
+export default App;
